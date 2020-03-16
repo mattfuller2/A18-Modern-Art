7590e824a2e7cc5546226d10edf581203d231d30
Matt Fuller <mdfulls@gmail.com>
2020-03-15 22:52:14 -0400

    Add files via upload

---
+++ ModernArt.java
@@ -0,0 +1,24 @@
+/**
+ * This prints characters that mimic a specific style of modern art.
+ *
+ * @author Matt Fuller
+ * @version CS 121-6 p0
+ */
+public class ModernArt {
+	/**
+	 * This will document the main method.
+	 */
+	public static void main(String[] args) {
+		System.out.println("*****************************");
+		System.out.println("*      *               *    *");
+		System.out.println("*      *               *    *");
+		System.out.println("*      *               *    *");
+		System.out.println("************************    *");
+		System.out.println("*      *               *    *");
+		System.out.println("*      *               *    *");
+		System.out.println("*      **********************");
+		System.out.println("*      *        *           *");
+		System.out.println("*      *        *           *");
+		System.out.println("*****************************");
+	}
+}
